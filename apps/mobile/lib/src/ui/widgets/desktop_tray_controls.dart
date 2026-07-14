2c8ed5a266abf721f2d920c0251bed4c31fdd20b
Yunus COGAL <cogalyunus@gmail.com>
2026-07-14 16:17:11 +0300

    fix: satisfy tray control analyzer

--- apps/mobile/lib/src/ui/widgets/desktop_tray_controls.dart
+++ apps/mobile/lib/src/ui/widgets/desktop_tray_controls.dart
@@ -1,10 +1,9 @@
 import 'dart:async';
 import 'dart:io';
-import 'dart:typed_data';
 import 'dart:ui' as ui;
 
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart' hide MenuItem;
+import 'package:flutter/material.dart';
 import 'package:path/path.dart' as path;
 import 'package:path_provider/path_provider.dart';
 import 'package:tray_manager/tray_manager.dart';
--- apps/mobile/test/desktop_tray_controls_test.dart
+++ apps/mobile/test/desktop_tray_controls_test.dart
@@ -1,5 +1,3 @@
-import 'dart:typed_data';
-
 import 'package:aethertune/src/ui/widgets/desktop_tray_controls.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
