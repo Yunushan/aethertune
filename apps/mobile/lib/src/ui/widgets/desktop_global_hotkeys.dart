b3751ae77dfd80b86abeaf49b39628c2cb518b17
Yunus COGAL <cogalyunus@gmail.com>
2026-07-14 15:36:31 +0300

    fix: import desktop hotkey widget types

--- apps/mobile/lib/src/ui/widgets/desktop_global_hotkeys.dart
+++ apps/mobile/lib/src/ui/widgets/desktop_global_hotkeys.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 import 'package:hotkey_manager/hotkey_manager.dart';
 
 bool supportsDesktopGlobalHotkeys(TargetPlatform platform) {
